d2e6f76b1f74c16d84c25615a281a2bcf3b6e14d
Tyree Franklin <tyree.franklinjr@icloud.com>
2025-02-16 16:58:38 -0600

    Initial Commit

---
+++ AeroAid/AeroAidApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroAidApp.swift
+//  AeroAid
+//
+//  Created by Tyree Franklin JR on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AeroAidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AeroAidTests/AeroAidTests.swift
@@ -0,0 +1,17 @@
+//
+//  AeroAidTests.swift
+//  AeroAidTests
+//
+//  Created by Tyree Franklin JR on 2/16/25.
+//
+
+import Testing
+@testable import AeroAid
+
+struct AeroAidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
