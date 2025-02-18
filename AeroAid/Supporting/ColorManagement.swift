f54a9a3b3f5a92cfa4e0770b6a269ff12aa613a4
Tyree Franklin <tyree.franklinjr@icloud.com>
2025-02-18 11:26:43 -0600

    Created a home foundation fie, and created the colormanagement file (testing in progress)

---
+++ AeroAid/Supporting/ColorManagement.swift
@@ -0,0 +1,18 @@
+//
+//  ColorManagement.swift
+//  AeroAid
+//
+//  Created by Tyree Franklin JR on 2/18/25.
+//
+
+import SwiftUI
+
+struct ColorManagement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorManagement()
+}
---
+++ AeroAid/UserInterface/Home/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  AeroAid
+//
+//  Created by Tyree Franklin JR on 2/18/25.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
