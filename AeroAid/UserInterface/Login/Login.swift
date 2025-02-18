997a320d5ab2da6fa04b52f6679c6a4827b8f453
Tyree Franklin <tyree.franklinjr@icloud.com>
2025-02-18 10:05:58 -0600

    Created a login file to code the authentication user intercace, will plug in the login file into the loginfoundation UI

---
+++ AeroAid/UserInterface/Login/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  AeroAid
+//
+//  Created by Tyree Franklin JR on 2/18/25.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
